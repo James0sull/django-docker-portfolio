1337b27d9d4a07e321aec0ba06b18ab6b05c83eb
James O'Sullivan <x00202782@mytudublin.ie>
2026-07-15 18:36:05 +0100

    Rename Django project configuration folder from myblogs to Portfolio_web

--- Portfolio_web/urls.py
+++ Portfolio_web/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myblogs project.
+URL configuration for Portfolio_web project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
